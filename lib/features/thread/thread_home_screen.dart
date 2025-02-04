38f540b8b67ccf68c349b00895475e461495d10e
hyejj19@naver.com <hyejj19@naver.com>
2025-02-04 23:26:22 +0900

    feat: nav bar select action & animation

--- lib/features/thread/thread_home_screen.dart
+++ lib/features/thread/thread_home_screen.dart
@@ -2,38 +2,65 @@ import 'package:flutter/material.dart';
 import 'package:flutter_study_ch/features/thread/widgets/nav_tab.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class ThreadHomeScreen extends StatelessWidget {
+class ThreadHomeScreen extends StatefulWidget {
   const ThreadHomeScreen({super.key});
 
   @override
+  State<ThreadHomeScreen> createState() => _ThreadHomeScreenState();
+}
+
+class _ThreadHomeScreenState extends State<ThreadHomeScreen> {
+  int _selectedIndex = 0;
+
+  void _onTap(int index) {
+    print(index);
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      body: Stack(
+        children: [
+          Offstage(),
+        ],
+      ),
       bottomNavigationBar: BottomAppBar(
         color: Colors.white,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            NavTab(
-              isSelected: false,
-              icon: FontAwesomeIcons.house,
-            ),
-            NavTab(
-              isSelected: false,
-              icon: FontAwesomeIcons.magnifyingGlass,
-            ),
-            NavTab(
-              isSelected: false,
-              icon: FontAwesomeIcons.penToSquare,
-            ),
-            NavTab(
-              isSelected: false,
-              icon: FontAwesomeIcons.heart,
-            ),
-            NavTab(
-              isSelected: false,
-              icon: FontAwesomeIcons.user,
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              NavTab(
+                isSelected: _selectedIndex == 0,
+                icon: FontAwesomeIcons.house,
+                onTap: () => _onTap(0),
+              ),
+              NavTab(
+                isSelected: _selectedIndex == 1,
+                icon: FontAwesomeIcons.magnifyingGlass,
+                onTap: () => _onTap(1),
+              ),
+              NavTab(
+                isSelected: _selectedIndex == 2,
+                icon: FontAwesomeIcons.penToSquare,
+                onTap: () => _onTap(2),
+              ),
+              NavTab(
+                isSelected: _selectedIndex == 3,
+                icon: FontAwesomeIcons.heart,
+                onTap: () => _onTap(3),
+              ),
+              NavTab(
+                isSelected: _selectedIndex == 4,
+                icon: FontAwesomeIcons.user,
+                onTap: () => _onTap(4),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/features/thread/widgets/nav_tab.dart
+++ lib/features/thread/widgets/nav_tab.dart
@@ -6,18 +6,21 @@ class NavTab extends StatelessWidget {
     super.key,
     required this.isSelected,
     required this.icon,
+    required this.onTap,
   });
 
   final bool isSelected;
   final IconData icon;
+  final Function onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
+    return GestureDetector(
+      onTap: () => onTap(),
       child: Center(
         child: AnimatedOpacity(
           opacity: isSelected ? 1 : 0.5,
-          duration: Duration(microseconds: 300),
+          duration: Duration(milliseconds: 100),
           child: FaIcon(
             icon,
             color: Colors.black,
